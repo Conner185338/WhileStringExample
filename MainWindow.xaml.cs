369426002ea0141249c7afe99db8d835afa49bae
Conner185338 <47399959+Conner185338@users.noreply.github.com>
2019-03-08 14:54:57 -0500

    Add files via upload
    added the files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Conner Warboys
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _185338WhileStringExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string myWord = "Hello";
+            string output = "";
+            while(myWord.Length > 0)
+            {
+                output += myWord.Substring(0, 1) + Environment.NewLine;
+                myWord = myWord.Substring(1);
+            }
+            lblOutput.Content = output;
+        }
+    }
+}
